96b12199bc246ffd80997c7e4b6409f2738ceb62
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-24 23:57:09 +0100

    (maint) Add unit tests for required description

---
+++ Source/chocolatey-language-server.Tests/Validations/DescriptionRequiredValidationTests.cs
@@ -0,0 +1,37 @@
+﻿using Chocolatey.Language.Server.Models;
+using Chocolatey.Language.Server.Validations;
+using NUnit.Framework;
+
+namespace Chocolatey.Language.Server.Tests.Validations
+{
+    public class DescriptionRequiredValidationTests : ValidationBaseTests<DescriptionRequiredValidation>
+    {
+        public override string ExpectedId { get; } = "choco0002";
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("                   ")]
+        public void Should_FailValidationWhenDescriptionIsNullOrWhitespace(string value)
+        {
+            var package = new Package
+            {
+                Description = value
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_NotFailValidationWhenDescriptionHaveText()
+        {
+            var package = new Package
+            {
+                Description = "My new awesome package description"
+            };
+
+            var result = Rule.Validate(package);
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
