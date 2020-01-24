ff9fa8c13bf8e245691eaeb97bded45ae51f78a9
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-24 23:55:33 +0100

    (maint) Add unit tests for minimum copyright length

---
+++ Source/chocolatey-language-server.Tests/Validations/CopyrightWordCountTests.cs
@@ -0,0 +1,46 @@
+﻿using Chocolatey.Language.Server.Models;
+using Chocolatey.Language.Server.Validations;
+using NUnit.Framework;
+
+namespace Chocolatey.Language.Server.Tests.Validations
+{
+    public class CopyrightWordCountTests : ValidationBaseTests<CopyrightWordCount>
+    {
+        public override string ExpectedId { get; } = "choco0006";
+
+        [Test]
+        public void Should_FailValidationWhenCharacterCountIsEqualTo4()
+        {
+            var package = new Package
+            {
+                Copyright = "2020"
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_FailValidationWhenCharacterCountIsLessThan4()
+        {
+            var package = new Package
+            {
+                Copyright = "© m"
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_NotFailValidationWhenCharacterCountIsMoreThan4()
+        {
+            var package = new Package
+            {
+                Copyright = "© 2020"
+            };
+
+            var result = Rule.Validate(package);
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
