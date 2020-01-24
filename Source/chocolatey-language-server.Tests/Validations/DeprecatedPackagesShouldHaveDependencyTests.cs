853858483e1fead68dc6926de36b36834a411b0d
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-24 23:56:09 +0100

    (maint) Add unit tests for deprecated pkg with dependency

---
+++ Source/chocolatey-language-server.Tests/Validations/DeprecatedPackagesShouldHaveDependencyTests.cs
@@ -0,0 +1,36 @@
+﻿using Chocolatey.Language.Server.Models;
+using Chocolatey.Language.Server.Validations;
+using NUnit.Framework;
+
+namespace Chocolatey.Language.Server.Tests.Validations
+{
+    public class DeprecatedPackagesShouldHaveDependencyTests : ValidationBaseTests<DeprecatedPackagesShouldHaveDependency>
+    {
+        public override string ExpectedId { get; } = "choco0007";
+
+        [Test]
+        public void Should_FailValidationWhenNoDependencyIsSpecified()
+        {
+            var package = new Package
+            {
+                Title = "[deprecated] Some kind of title"
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_NotFailValidationWhenADependencyIsSpecified()
+        {
+            var package = new Package
+            {
+                Title = "[deprecated] Some kind of title"
+            };
+            package.AddDependency(new Dependency { Id = "some-package-id" });
+
+            var result = Rule.Validate(package);
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
