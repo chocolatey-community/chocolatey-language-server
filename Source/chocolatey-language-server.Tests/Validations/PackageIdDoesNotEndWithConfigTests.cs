47b6b51dffa75a2fb3b72ebdbb59951ee070dfd4
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-25 02:33:06 +0100

    (maint) Add unit tests for package id ending with config

---
+++ Source/chocolatey-language-server.Tests/Validations/PackageIdDoesNotEndWithConfigTests.cs
@@ -0,0 +1,33 @@
+﻿using Chocolatey.Language.Server.Models;
+using Chocolatey.Language.Server.Validations;
+using NUnit.Framework;
+
+namespace Chocolatey.Language.Server.Tests.Validations
+{
+    public class PackageIdDoesNotEndWithConfigTests : ValidationBaseTests<PackageIdDoesNotEndWithConfig>
+    {
+        public override string ExpectedId { get; } = "choco0009";
+
+        [Test]
+        public void Should_FailValidationWhenPackageIdEndsWithConfig()
+        {
+            var package = new Package
+            {
+                Id = "my-my.config",
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_NotFailValidationWhenPackageIdDoesNotEndWithConfig()
+        {
+            var package = new Package
+            {
+                Id = "my-my",
+            };
+
+            ValidateDiagnosticResult(package, 0);
+        }
+    }
+}
