9a04c4d7f660667d34797350b0590553be930eb2
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-25 02:38:24 +0100

    (maint) Add unit tests for underscore in package id

---
+++ Source/chocolatey-language-server.Tests/Validations/PackageIdUsesUnderscoresTests.cs
@@ -0,0 +1,33 @@
+﻿using Chocolatey.Language.Server.Models;
+using Chocolatey.Language.Server.Validations;
+using NUnit.Framework;
+
+namespace Chocolatey.Language.Server.Tests.Validations
+{
+    public class PackageIdUsesUnderscoresTests : ValidationBaseTests<PackageIdUsesUnderscores>
+    {
+        public override string ExpectedId { get; } = "choco3001";
+
+        [Test]
+        public void Should_FailValidationWhenPackageIdContainsUnderscore()
+        {
+            var package = new Package
+            {
+                Id = "my_awesome_package_id",
+            };
+
+            ValidateDiagnosticResult(package, 1);
+        }
+
+        [Test]
+        public void Should_NotFailValidationWhenPackageIdDoesNotContainUnderscore()
+        {
+            var package = new Package
+            {
+                Id = "again-my-awesome-package-id",
+            };
+
+            ValidateDiagnosticResult(package, 0);
+        }
+    }
+}
