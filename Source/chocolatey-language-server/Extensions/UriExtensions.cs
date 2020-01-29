83b2b43baf018c8ad3a2817d10e9b20bd94a7900
Maurice Kevenaar <maurice@kevenaar.name>
2020-01-29 09:39:51 +0100

    (GH-8) Back sync URL validation changes
    This updates the UriExtensions with the changes currenly available in
    the package-verifier.

--- Source/chocolatey-language-server/Extensions/UriExtensions.cs
+++ Source/chocolatey-language-server/Extensions/UriExtensions.cs
@@ -15,23 +15,60 @@ namespace Chocolatey.Language.Server.Extensions
         /// <param name="useGetMethod">Bool Switch to GET request.</param>
         public static bool IsValid(this Uri url, bool useGetMethod = false)
         {
-            var request = (HttpWebRequest)WebRequest.Create(url);
+            if (url == null)
+            {
+                return true;
+            }
 
-            //This would allow 301 and 302 to be valid as well
-            request.AllowAutoRedirect = true;
-            request.Timeout = 15000;
-            if (!useGetMethod) request.Method = WebRequestMethods.Http.Head;
+            if (url.Scheme == "mailto")
+            {
+                // mailto links are not expected/allowed, therefore immediately fail with no further processing
+                return false;
+            }
+
+            if (!url.Scheme.StartsWith("http"))
+            {
+                // Currently we can only validate http/https URL's, therefore simply return true for any others.
+                return true;
+            }
 
             try
             {
-                using (var response = (HttpWebResponse)request.GetResponse())
+                var request = (HttpWebRequest) WebRequest.Create(url);
+                var cookieContainer = new CookieContainer();
+
+                request.CookieContainer = cookieContainer;
+                //This would allow 301 and 302 to be valid as well
+                request.AllowAutoRedirect = true;
+                request.Timeout = 15000;
+                if (!useGetMethod) request.Method = WebRequestMethods.Http.Head;
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36";
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+
+                using (var response = (HttpWebResponse) request.GetResponse())
                 {
                     return response.StatusCode == HttpStatusCode.OK;
                 }
             }
-            catch (WebException)
+            catch (WebException ex)
             {
+                if (ex.Status == WebExceptionStatus.SecureChannelFailure)
+                {
+                    //Since this is likely due to missing Ciphers on the machine running the language server, this URL will be marked as valid for the time being.
+                    return true;
+                }
+
+                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Message == "The remote server returned an error: (503) Server Unavailable.")
+                {
+                    //This could be due to Cloudflare DDoS protection acting in front of the site, or another valid reason, as such, this URL will be marked as valid for the time being.
+                    return true;
+                }
+
                 return !useGetMethod && url.IsValid(true);
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }
 
